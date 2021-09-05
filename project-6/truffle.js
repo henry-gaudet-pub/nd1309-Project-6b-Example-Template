86193287858de63b9c6da8b42f0decb7fd94f6b8
henry <henrygaudet88@gmail.com>
2021-09-04 23:01:03 -0500

    deployed to rinkeby

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,9 +1,19 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const fs = require('fs');
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "1337"
+    },
+    rinkeby: {
+      provider: ()=> {
+        //replace string parameters
+        return new HDWalletProvider('spirit supply whale amount human item harsh scare congress discover talent hamster', 'https://rinkeby.infura.io/v3/7c244ff134814e02a57bf32a4af11289');
+      },
+      network_id: 4
     }
   }
 };
